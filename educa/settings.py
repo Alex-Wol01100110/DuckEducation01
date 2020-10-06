8aa9070e27f086ec564ca146b341dadd96f057c7
jack <3hellspawn3@gmail.com>
2020-10-06 22:18:58 +0300

    secret key test for deployment

--- educa/settings.py
+++ educa/settings.py
@@ -11,18 +11,15 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from dotenv import load_dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-load_dotenv(verbose=True, dotenv_path=os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '#mj!6*zn%n-!ty97m6j%!8p2x6owq9m2ftkxzhvfehxf9t@pmq')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
