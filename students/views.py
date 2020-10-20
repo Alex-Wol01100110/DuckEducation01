f0339843b6a4d059e7f23901543832fd82736444
jack <3hellspawn3@gmail.com>
2020-10-20 15:42:19 +0300

    Deleted old registration view and template

--- students/views.py
+++ students/views.py
@@ -104,19 +104,6 @@ class NewPasswordResetView(PasswordResetView):
 class NewPasswordResetConfirmView(PasswordResetConfirmView):
     success_url = reverse_lazy('students:password_reset_complete')
 
-class StudentRegistrationView(CreateView):
-    template_name = 'students/student/registration.html'
-    form_class = UserCreationForm
-    success_url = reverse_lazy('students:student_course_list')
-    
-    def form_valid(self, form):
-        result = super().form_valid(form)
-        cd = form.cleaned_data
-        user = authenticate(username=cd['username'],
-                            password=cd['password1'])
-        login(self.request, user)
-        return result
-
 class StudentEnrollCourseView(LoginRequiredMixin, FormView):
     course = None
     form_class = CourseEnrollForm
